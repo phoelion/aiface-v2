a7c1a8df60670476b8f68bf6e828576d38190dd4
Ali Sadeghi <phoeliondev@gmail.com>
2025-03-17 13:31:52 +0330

    goh to py

--- swapper/app.py
+++ swapper/app.py
@@ -13,44 +13,32 @@ from src.DataManager.utils import imread_rgb
 
 
 def run_application(config: DictConfig):
-    id_image_path = Path(config.id_image)
+    try:
+        id_image_path = Path(config.id_image)
+        att_image_path = Path(config.att_image)
+        output_dir = Path(config.output_dir)
+        assert id_image_path.exists(), f"Can't find {id_image_path} file!"
+        id_image: Optional[np.ndarray] = imread_rgb(id_image_path)
+        att_image: Optional[ImageDataManager] = None
+        if att_image_path and (att_image_path.is_file() or att_image_path.is_dir()):
+            att_image: Optional[ImageDataManager] = ImageDataManager(
+                src_data=att_image_path, output_dir=output_dir
+            )
+        att_video: Optional[VideoDataManager] = None
+        assert not (att_video and att_image), "Only one attribute source can be used!"
+        data_manager = att_video if att_video else att_image
+        model = SimSwap(
+            config=config,
+            id_image=id_image,
 
-    att_image_path = Path(config.att_image)
-
-    output_dir = Path(config.output_dir)
-
-    assert id_image_path.exists(), f"Can't find {id_image_path} file!"
-
-    id_image: Optional[np.ndarray] = imread_rgb(id_image_path)
-
-    att_image: Optional[ImageDataManager] = None
-    if att_image_path and (att_image_path.is_file() or att_image_path.is_dir()):
-        att_image: Optional[ImageDataManager] = ImageDataManager(
-            src_data=att_image_path, output_dir=output_dir
         )
-
-    att_video: Optional[VideoDataManager] = None
-
-    assert not (att_video and att_image), "Only one attribute source can be used!"
-
-    data_manager = att_video if att_video else att_image
-
-    model = SimSwap(
-        config=config,
-        id_image=id_image,
-    )
-
-    filename = ""
-
-    for _ in tqdm(range(len(data_manager))):
-        att_img = data_manager.get()
-
-        output = model(att_img)
-
-        result = data_manager.save(output)
-        filename = result
-
-    return filename
+        for _ in tqdm(range(len(data_manager))):
+            att_img = data_manager.get()
+            output = model(att_img)
+            result =data_manager.save(output)
+        return True
+    except Exception as e:
+        return e
 
 
 if __name__ == "__main__":
