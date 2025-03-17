47713084fd2b1a86c942105f8a6b53750b538139
Ali Sadeghi <phoeliondev@gmail.com>
2025-03-17 13:06:34 +0330

    add watermark adder

--- swapper/crop.py
+++ swapper/crop.py
@@ -1,128 +1,29 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 import os
 
-
-def cropper(image_name: str):
+def watermark_adder (image_name:str,save_path:str):
     try:
-        img_path = os.path.abspath(
-            os.path.join(os.getcwd(), "..", "aiface", "faceswap", "img", image_name)
-        )
-        img = Image.open(img_path)
-        w, h = img.size
-        r = 0
-        if w / h > 1:
-            r = (h * 12) / 100
-        elif w / h < 1:
-            r = (h * 5) / 100
-        else:
-            r = (h * 7) / 100
-        box = (0, 0, w, h - r)
-        img2 = img.crop(box)
-        final_path = os.path.abspath(
-            os.path.join(os.getcwd(), "..", "aiface", "faceswap", image_name)
-        )
-        img2.save(final_path)
+        img_path=(os.path.abspath(os.path.join(os.getcwd(),'..',"public","img",image_name)))
+        id_image = Image.open(img_path)
+        text = "faceswapperonline.com"
+        font = ImageFont.truetype("arial.ttf", 50)
+        image_resolution = id_image.size
+        box_width = image_resolution[0] // 3
+        box_height = box_width // 6
+        text_size = box_height // 2
+        font = ImageFont.truetype("arial.ttf", text_size)
+        x = image_resolution[0] - box_width - 10
+        y = image_resolution[1] - box_height - 10
+        watermark_box = Image.new('RGBA', (box_width, box_height), (0, 0, 0, 0))
+        draw = ImageDraw.Draw(watermark_box)
+        draw.text((box_width * 0.070, box_height * 0.25), text, fill=(255, 255, 255), font=font)
+        id_image.paste(watermark_box, (x, y), watermark_box)
+        final_path=os.path.abspath(os.path.join(os.getcwd(),'..',"public",image_name))
+        id_image.save(final_path)
+        id_image.close()
         return image_name
-    except:
+    except BaseException as error :
+        print('An exception occurred: {}'.format(error))
         return False
 
-
-# def add_watermark(original_image_name, output_image_name, watermark):
-#     try:
-#         # watermark_size = (200, 100)
-#         original_image_path = os.path.abspath(
-#             os.path.join(os.getcwd(), "..", "aiface", "faceswap", original_image_name)
-#         )
-#         watermark_path = os.path.abspath(
-#             os.path.join(os.getcwd(), "watermark", watermark)
-#         )
-#         output_image_path = os.path.abspath(
-#             os.path.join(os.getcwd(), "..", "aiface", "faceswap", original_image_name)
-#         )
-
-#         # Open the original image and the watermark image using PIL
-#         original_image = Image.open(original_image_path)
-#         watermark = Image.open(watermark_path)
-
-#         # Make sure the watermark image has an alpha channel (transparency)
-#         if watermark.mode != "RGBA":
-#             watermark = watermark.convert("RGBA")
-
-#         # Resize the watermark image to the specified size
-#         # watermark = watermark.resize(watermark_size, Image.LANCZOS)
-
-#         # Create a new image with the original content
-#         watermarked_image = Image.new("RGBA", original_image.size, (0, 0, 0, 0))
-
-#         # Calculate the position to paste the watermark (bottom right corner)
-#         position = (
-#             original_image.width - watermark.width,
-#             original_image.height - watermark.height,
-#         )
-
-#         # Blend the original image and the watermark image
-#         watermarked_image.paste(original_image, (0, 0))
-#         watermarked_image.paste(watermark, position, mask=watermark)
-
-#         # Save the watermarked image to the specified output path
-#         watermarked_image.save(output_image_path, format="PNG")
-
-#         return output_image_name
-#     except Exception as e:
-#         print(f"Error adding watermark: {e}")
-
-def add_watermark(original_image_name, output_image_name, watermark):
-    try:
-        original_image_path = os.path.abspath(
-            os.path.join(os.getcwd(), "..", "aiface", "faceswap", original_image_name)
-        )
-        watermark_path = os.path.abspath(
-            os.path.join(os.getcwd(), "watermark", watermark)
-        )
-        output_image_path = os.path.abspath(
-            os.path.join(os.getcwd(), "..", "aiface", "faceswap", output_image_name)
-        )
-
-        # Open the original image and the watermark image using PIL
-        original_image = Image.open(original_image_path)
-        watermark = Image.open(watermark_path)
-
-        # Make sure the watermark image has an alpha channel (transparency)
-        if watermark.mode != "RGBA":
-            watermark = watermark.convert("RGBA")
-
-        # Calculate the scaling factor based on the smaller dimension ratio
-        width_ratio = original_image.width / watermark.width
-        height_ratio = original_image.height / watermark.height
-        scaling_factor = min(width_ratio, height_ratio) * 0.15  # This is 10% of the smaller dimension ratio
-
-        # Calculate new watermark size while maintaining the aspect ratio
-        watermark_size = (
-            int(watermark.width * scaling_factor),
-            int(watermark.height * scaling_factor),
-        )
-
-        # Resize the watermark image
-        watermark = watermark.resize(watermark_size, Image.LANCZOS)
-
-        # Create a new image with the original content
-        watermarked_image = Image.new("RGBA", original_image.size, (0, 0, 0, 0))
-
-        # Calculate the position to paste the watermark (bottom right corner)
-        position = (
-            original_image.width - watermark.width,
-            original_image.height - watermark.height,
-        )
-
-        # Blend the original image and the watermark image
-        watermarked_image.paste(original_image, (0, 0))
-        watermarked_image.paste(watermark, position, mask=watermark)
-
-        # Save the watermarked image to the specified output path
-        watermarked_image.save(output_image_path, format="PNG")
-
-        return output_image_name
-    except Exception as e:
-        print(f"Error adding watermark: {e}")
-        return False
